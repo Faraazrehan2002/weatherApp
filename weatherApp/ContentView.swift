f1cedc5836f4fa150d0e863080e654735e48bd1a
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-09-07 14:11:41 -0500

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 9/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
