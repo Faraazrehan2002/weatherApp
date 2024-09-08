15a9b6b03d1267081f2e41a1ceb9327b7963f81a
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-09-08 12:41:36 -0500

    Added EditViewScreen

---
+++ weatherApp/EditSeasonView.swift
@@ -0,0 +1,41 @@
+//
+//  EditSeasonView.swift
+//  weatherApp
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 9/8/24.
+//
+
+import SwiftUI
+
+struct EditSeasonView: View {
+    var body: some View {
+        
+        HStack {
+            Button(action: {}, label: {Text("Cancel")})
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Done")
+            })
+        }.padding()
+        
+        VStack{
+            Text("Update Season Name")
+                .bold()
+                .font(.largeTitle)
+            
+            TextField("Edit Season", text: .constant("Default"))
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .autocorrectionDisabled()
+        }
+        
+        Spacer()
+        
+    }
+}
+
+#Preview {
+    EditSeasonView()
+}
